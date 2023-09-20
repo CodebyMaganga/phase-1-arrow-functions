7520dbbc525f5e430e5db5078f2b44b9ec45eaf3
Maganga Mwambonu <codebymaganga@gmail.com>
2023-09-20 22:39:05 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+let divide = function (){
+    return 2000 / 100
+} 
+
+console.log(divide())
+
+let square = x => x * x
+
+console.log(square(3))
+
+let add = (x, y) => x + y
+
+console.log(add(4, 5))
